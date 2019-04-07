48fb86966a4587d2e6116f3585a34db923a1e40e
gang.yan <iamyangang@126.com>
2019-04-07 21:59:11 +0800

    first commit

---
+++ src/main/java/com/spark/spring/boot/fileserver/repository/FileRepository.java
@@ -0,0 +1,15 @@
+package com.spark.spring.boot.fileserver.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.spark.spring.boot.fileserver.domain.File;
+ 
+
+/**
+ * File 存储库.
+ * 
+ * @since 1.0.0 2017年3月28日
+ * @author
+ */
+public interface FileRepository extends MongoRepository<File, String> {
+}
