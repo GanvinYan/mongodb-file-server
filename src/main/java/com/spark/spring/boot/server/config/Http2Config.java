94d14967f2104bb57e77c6d35187336af7e05876
gang.yan <gang.yan@newtouch.cn>
2019-07-04 18:54:18 +0800

    http2 问题修改 改为可启动

--- src/main/java/com/spark/spring/boot/server/config/Http2Config.java
+++ src/main/java/com/spark/spring/boot/server/config/Http2Config.java
@@ -6,41 +6,52 @@ import io.undertow.servlet.api.SecurityConstraint;
 import io.undertow.servlet.api.SecurityInfo;
 import io.undertow.servlet.api.TransportGuaranteeType;
 import io.undertow.servlet.api.WebResourceCollection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class Http2Config {
+
+
     /**
-     * undertow服务器下http重定向到https
+     * https服务端口
+     */
+    @Value("${server.port}")
+    private Integer httpsPort;
+
+    /**
+     * 采用Undertow作为服务器。
+     * Undertow是一个用java编写的、灵活的、高性能的Web服务器，提供基于NIO的阻塞和非阻塞API，特点：
+     * 非常轻量级，Undertow核心瓶子在1Mb以下。它在运行时也是轻量级的，有一个简单的嵌入式服务器使用少于4Mb的堆空间。
+     * 支持HTTP升级，允许多个协议通过HTTP端口进行多路复用。
+     * 提供对Web套接字的全面支持，包括JSR-356支持。
+     * 提供对Servlet 3.1的支持，包括对嵌入式servlet的支持。还可以在同一部署中混合Servlet和本机Undertow非阻塞处理程序。
+     * 可以嵌入在应用程序中或独立运行，只需几行代码。
+     * 通过将处理程序链接在一起来配置Undertow服务器。它可以对各种功能进行配置，方便灵活。
      */
     @Bean
-    UndertowServletWebServerFactory undertowServletWebServerFactory() {
-        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
-        factory.addBuilderCustomizers(
-                builder -> {
-                    builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true)
-                            .setServerOption(UndertowOptions.HTTP2_SETTINGS_ENABLE_PUSH, true);
-                });
-//       这段可以增加http重定向，如果只需要http2的话下面的代码可以去掉
-        factory.addBuilderCustomizers(new UndertowBuilderCustomizer() {
-            @Override
-            public void customize(Undertow.Builder builder) {
-                builder.addHttpListener(8080, "0.0.0.0");
-            }
+    public ServletWebServerFactory undertowFactory() {
+        UndertowServletWebServerFactory undertowFactory = new UndertowServletWebServerFactory();
+        undertowFactory.addBuilderCustomizers((Undertow.Builder builder) -> {
+            builder.addHttpListener(8080, "0.0.0.0");
+            // 开启HTTP2
+            builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true);
         });
-//      下面这段是将http的8080端口重定向到https的8081端口上
-        factory.addDeploymentInfoCustomizers(deploymentInfo -> {
+        undertowFactory.addDeploymentInfoCustomizers(deploymentInfo -> {
+            // 开启HTTP自动跳转至HTTPS
             deploymentInfo.addSecurityConstraint(new SecurityConstraint()
-                    .addWebResourceCollection(new WebResourceCollection()
-                            .addUrlPattern("/*")).setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
+                    .addWebResourceCollection(new WebResourceCollection().addUrlPattern("/*"))
+                    .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
                     .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
-                    .setConfidentialPortManager(exchange -> 8081);
+                    .setConfidentialPortManager(exchange -> httpsPort);
         });
-        return factory;
+        return undertowFactory;
     }
+
 
 }
